20cf6300341472c23ad9fdd7ce3461551a4fe9d6
Jim Meyer <jim@geekdaily.org>
2016-08-08 22:29:51 -0700

    Modernize the spec rig

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,10 @@ require 'subtle'
 require 'contrast'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/panic_board_data')
 require 'mocha/setup'
+
+# Enable both should and expect syntax
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
